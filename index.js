95344e6df01fb934b1c6206415652856fcfbac2e
Kevin Arellano <KevinArellano@eHawkSolutions.Com>
2022-04-29 13:01:52 -0400

    Updated Action function

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+
+async function checkFileExists(filePath) {
+    return fs.promise.access(filePath)
+    .then(() => {
+        core.info(`File ${filePath} exists.`);
+        return true;
+    })
+    .catch(() => {
+        core.setFailed(`File ${filePath} is manditory.`);
+        return false;
+    });
+}
 
 (
     async function () {
         try {
             core.notice("Calling our action.");
+            checkFileExists("LICENSE");
+            checkFileExists("READEME.md");
         } catch (error) {
             core.setFailed(error.message);
         }
